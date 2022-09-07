2c35c0b4bd7c1eaaba50a927ea581c594fef895d
alyssa_robinson <arobinson0227@gmail.com>
2022-09-06 21:23:51 -0400

    Commit scipt_1

--- script_1.py
+++ script_1.py
@@ -1,12 +1,13 @@
 # Take in a list data structure as an input
-user_list = []
-user = (input('Enter a list of numbers: '))
-user_list.append(user)
+user_list = [] # Intializes the list
+user = (input('Enter a list of numbers: ')) # This allows the user to input the list of numbers
+user_list.append(user) # adds on to the list
 
 # Write a function to get unique values
 def get_unique_list(user_list):
-    unique_list = []
-    for i in user_list:
+    unique_list = []  # Initialize the unique list 
+# Use a for loop to go through individual elements in the list
+    for i in user_list: 
         if i not in unique_list:
             unique_list.append(i)
 # print the unique list
